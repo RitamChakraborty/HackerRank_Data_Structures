cd7e798cd9e51c0d8c0af2248e7c117ef9defc38
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2020-01-30 23:39:17 +0530

    Solve Arrays - DS

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # [HackerRank Data Structures](https://www.hackerrank.com/domains/data-structures)
+
+## Arrays
+- [Arrays - DS](https://www.hackerrank.com/challenges/arrays-ds/problem)
---
+++ src/arrays/arrays_ds/Solution.java
@@ -0,0 +1,23 @@
+// https://www.hackerrank.com/challenges/arrays-ds/problem
+
+package arrays.arrays_ds;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+		int n = scanner.nextInt();
+		int[] arr = new int[n];
+		
+		for (int i = 0; i < n; ++i) {
+			arr[i] = scanner.nextInt();
+		}
+		
+		for (int i = n - 1; i >= 0; --i) {
+			System.out.print(arr[i] + " ");
+		}
+	}
+}

cd94be29518dcfa8ce6405418c02c5b0bd070b34
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2021-04-23 13:29:04 +0530

    Print in Reverse

--- README.md
+++ README.md
@@ -14,3 +14,4 @@
 - [Insert a Node at the Head of a Linked List](https://www.hackerrank.com/challenges/insert-a-node-at-the-head-of-a-linked-list/problem)
 - [Insert a node at a specific position in a linked list](https://www.hackerrank.com/challenges/insert-a-node-at-a-specific-position-in-a-linked-list/problem)
 - [Delete a Node](https://www.hackerrank.com/challenges/delete-a-node-from-a-linked-list/problem)
+- [Print in Reverse](https://www.hackerrank.com/challenges/print-the-elements-of-a-linked-list-in-reverse/problem)
---
+++ src/linked_list/print_the_elements_of_a_linked_list_in_reverse/Solution.java
@@ -0,0 +1,98 @@
+package linked_list.print_the_elements_of_a_linked_list_in_reverse;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep) {
+        while (node != null) {
+            System.out.print(node.data);
+
+            node = node.next;
+
+            if (node != null) {
+                System.out.print(sep);
+            }
+        }
+    }
+
+    static void reversePrint(SinglyLinkedListNode head) {
+        if (head.next != null) {
+            SinglyLinkedListNode ptr = head.next;
+            SinglyLinkedListNode head1 = new SinglyLinkedListNode(head.data);
+
+            while (ptr != null) {
+                SinglyLinkedListNode node = new SinglyLinkedListNode(ptr.data);
+                node.next = head1;
+                head1 = node;
+                ptr = ptr.next;
+            }
+
+            SinglyLinkedListNode ptr1 = head1;
+
+            while (ptr1 != null) {
+                System.out.println(ptr1.data);
+                ptr1 = ptr1.next;
+            }
+        } else {
+            System.out.println(head.data);
+        }
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int tests = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int testsItr = 0; testsItr < tests; testsItr++) {
+            SinglyLinkedList llist = new SinglyLinkedList();
+
+            int llistCount = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = 0; i < llistCount; i++) {
+                int llistItem = scanner.nextInt();
+                scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+                llist.insertNode(llistItem);
+            }
+
+            reversePrint(llist.head);
+        }
+
+        scanner.close();
+    }
+}
